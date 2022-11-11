663a3ae65a7a036f115effc0c70a52300619b373
dmirak <dmirakian1023@gmail.com>
2022-11-10 19:21:03 -0500

    adds data service

--- SQL/BuildDatabase.sql
+++ SQL/BuildDatabase.sql
@@ -12,30 +12,28 @@ DROP TABLE IF EXISTS CodeBuilding;
 
 --Links building names to building codes
 CREATE TABLE CodeBuilding (
-    code varchar(5) PRIMARY KEY,
-    name varchar(20),
+    code varchar(10) PRIMARY KEY,
+    name varchar(50),
 );
-
 
 --Contains room number and building code for each room/question
 CREATE TABLE Room (
     IDRoom integer PRIMARY KEY,
-    buildingCode varchar(5) REFERENCES CodeBuilding(code),
-    roomNumber varchar(10),
+    buildingCode varchar(10) REFERENCES CodeBuilding(code),
+    roomNumber varchar(50),
 );
-
 
 --Record for each picture, links to Room. Will either contian filepath to image or contain the image itself
 CREATE TABLE ImageLink (
     IDImageLink integer PRIMARY KEY,
     roomID integer REFERENCES Room(IDRoom),
-    imagePath varchar(30)
+    image varbinary(max)
 );
 
 --Player from the scoreboard
 CREATE TABLE Player (
     IDPlayer integer PRIMARY KEY,
-    name varchar(10),
+    name varchar(30),
     score integer
 );
 
